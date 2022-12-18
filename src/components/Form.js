2369d2799c60eda74cc7f9f86643868bd8f4cb50
petarL1 <plefteroski@gmail.com>
2022-12-19 00:30:30 +0100

    DID THE INITIAL DESIGN TODO:CSS

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Form(props) {
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.addTask("Say hello!");
+      }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          Type your task
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
+}
