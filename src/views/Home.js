9e0e28e172d69d1d9062c94b2225f00a55f18719
petarL1 <plefteroski@gmail.com>
2023-03-02 12:35:46 +0100

    minor

--- src/views/Home.js
+++ src/views/Home.js
@@ -76,7 +76,7 @@ function Home(){
                 <div className="seperator"/>
                 <div className="section-3">
                 <div className="sec3-img">
-                    <img src="https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="About Us"></img>
+                    <img src="https://st.depositphotos.com/1003593/3947/i/450/depositphotos_39479909-stock-photo-about-us-blue-marker.jpg" alt="About Us"></img>
                 </div>
                 <div className="sec3-text">
                     <p>
